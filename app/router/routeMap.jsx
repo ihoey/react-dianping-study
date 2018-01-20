dd9f9a04a30c49a3dce5ea9c3acdcf894aa34a01
ihoey <mail@ihoey.com>
2018-01-20 16:06:01 +0800

    搜索完成

--- app/router/routeMap.jsx
+++ app/router/routeMap.jsx
@@ -5,7 +5,7 @@ import App from '../containers';
 import Home from '../containers/Home';
 import City from '../containers/City';
 // import User from '../containers/User';
-// import Search from '../containers/Search';
+import Search from '../containers/Search';
 // import Detail from '../containers/Detail';
 import NotFound from '../containers/404';
 
@@ -20,7 +20,7 @@ class RouterMap extends React.Component {
                     <IndexRoute component={Home} />
                     <Route path='/city' component={City} />
                     {/* <Route path='/User' component={User} /> */}
-                    {/* <Route path='/search/:type(/:keyword)' component={Search} /> */}
+                    <Route path='/search/:category(/:keyword)' component={Search} />
                     {/* <Route path='/detail/:id' component={Detail} /> */}
                     <Route path='*' component={NotFound} />
                 </Route>
